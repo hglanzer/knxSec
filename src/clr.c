4a2f8b14624607d0ab97498f69cd53968a7771de
hglanzer <harald.glanzer@gmail.com>
2015-06-29 23:40:51 +0200

    ..

--- src/clr.c
+++ src/clr.c
@@ -33,9 +33,10 @@ void clrWR(void *threadEnv)
 	int indCntTmp;
 	while(1)
 	{
-		pthread_mutex_lock(&globalMutex);
 		printf("CLR-WR: got input ");
 		rc = read(thisEnv->SECs2ClrPipe[READEND], &buffer[0], BUFSIZE);	// FIXME - non-blocking
+		
+		pthread_mutex_lock(&globalMutex);
 		indCntTmp = str2CtrInt(&buffer[0]);
 
 
